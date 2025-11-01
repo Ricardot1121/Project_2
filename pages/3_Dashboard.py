37c573f7e3f14a8be568234fd2de4c378f0879c4
PhilPhronesius <ephrones@msudenver.edu>
2025-11-01 17:10:32 -0600

    completed code for bar graph

--- pages/3_Dashboard.py
+++ pages/3_Dashboard.py
@@ -19,3 +19,20 @@ st.title("Cost($) of Video Game Development")
 st.caption("The development cost of well-received video games in the past decade (2015 - 2024).")
 
 df = pd.read_csv("data/top10.csv")
+
+median_cost = df["Cost of Development"].median()
+df["Cost of Development"].fillna(median_cost, inplace = True)
+
+if "GOTY" in df.columns:
+    df["Color"] = df{"GOTY"].apply(lambda x: "gold" if x else "blue")
+else:
+    df["Color"] = "blue"
+
+st.write(df.head())
+
+fig = px.bar(df, x = "Game Title", y = "Development Cost($)", color = "Color",
+             title = "Development Costs of Video Games",
+             labels = {"Video Games": "Video Games", "Cost of Development": "Development Cost($)"},
+             color_map = {"gold": "gold", "blue": "blue"])
+
+st.plotly_chart(fig, use_container_width=True)
