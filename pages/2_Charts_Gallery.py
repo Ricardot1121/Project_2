352a8f9301c05f072222fcfdb52fda5fb07c2fd3
PhilPhronesius <ephrones@msudenver.edu>
2025-11-08 18:50:05 -0700

    added hover to bar and line. added filter to sales

--- pages/2_Charts_Gallery.py
+++ pages/2_Charts_Gallery.py
@@ -23,16 +23,17 @@ df = pd.read_csv("data/top10.csv")
 median_cost = df["Cost of Development"].median()
 df["Cost of Development"].fillna(median_cost, inplace = True)
 
-df["Color"] = df{"GOTY"].apply(lambda x: "gold" if x == 1 else "blue")
+df["Color"] = df["GOTY"].apply(lambda x: "gold" if x == 1 else "blue")
 
 st.write(df.head())
 
-fig = px.bar(df, x = "Development Cost($)", y = "Game Title", color = "Color",
+fig_cost = px.bar(df, x = "Cost of Development", y = "Video Games", color = "Color",
              title = "Development Costs of Video Games",
              labels = {"Video Games": "Video Games", "Cost of Development": "Development Cost($)"},
-             color_map = {"gold": "gold", "blue": "blue"])
+             color_discrete_map = {"gold": "gold", "blue": "blue"},
+             hover_name = "Video Games", hover_data = ["Cost of Development"])
 
-st.plotly_chart(fig, use_container_width=True)
+st.plotly_chart(fig_cost, use_container_width=True)
 
 sales_df = pd.read_csv("10salehistory.csv")
 
@@ -45,6 +46,11 @@ for col in sales_df.columns[1:]:
     sales_df[col] = sales_df[col].apply(convert_sales)
 
 sales_df_melted = salesdf.melt(id_vars = ["Video Games"], var_name = "Year", value_names = "Units Sold")
+
+year_options = sorted(sales_df_melted["Year"].unique())
+selected_year = st.selectbox("Select Year", year_options)
+
+filtered_sales_df = sales_df_melted[sales_df_melted["Year"] == selected_year]
 
 fis_sales = px.line(sales_df_melted, x = "Year", y = "Units Sold", color = "Video Games",
                     title = "Sales Over Time (2015 - 2024)",
